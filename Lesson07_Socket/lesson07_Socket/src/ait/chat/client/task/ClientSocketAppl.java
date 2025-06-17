84d5f87917d4546a0f1a8ac0b6b23f39bd47f9a3
Pavel Nesnamov <p.nesnamov@gmail.com>
2025-06-17 11:03:54 +0200

    all fixed

---
+++ Lesson07_Socket/lesson07_Socket/src/ait/chat/client/task/ClientSocketAppl.java
@@ -0,0 +1,26 @@
+package ait.chat.client.task;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientSocketAppl {
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            args = new String[]{args[0], "9000"};
+        }
+        if (args.length == 0) {
+            args = new String[]{"127.0.0.1", "9000"};
+        }
+        String serverHost = args[0]; 
+        int port = Integer.parseInt(args[1]);
+        
+        try {
+            Socket socket = new Socket(serverHost, port);
+            new MessageReceiver(socket);
+            new Thread(new MessageSender(socket)).start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Lesson07_Socket/lesson07_Socket/src/ait/chat/client/task/MessageSender.java
@@ -0,0 +1,34 @@
+package ait.chat.client.task;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class MessageSender implements Runnable {
+    private final Socket socket;
+
+    public MessageSender(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (Socket socket = this.socket) {
+            PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
+            Scanner consoleScanner = new Scanner(System.in);
+            System.out.println("Enter your name");
+            String name = consoleScanner.nextLine();
+            System.out.println("Enter your message, or type exit for quit");
+            String message = consoleScanner.nextLine();
+            while (!"exit".equalsIgnoreCase(message)) {
+                socketWriter.printf("%s [%s] %s\n", name, LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm")), message);
+                message = consoleScanner.nextLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
